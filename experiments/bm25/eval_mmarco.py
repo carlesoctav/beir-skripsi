b495aedcadc42e0f6e2a7de3ab7701911f00af7d
carlesoctav <carlesoctavianus@tuta.io>
2023-10-14 06:51:56 +0000

    rapi-rapi

--- experiments/bm25/eval_mmarco.py
+++ experiments/bm25/eval_mmarco.py
@@ -7,59 +7,33 @@ import pathlib, os, random
 import logging
 from pyprojroot import here
 
-#### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-#### /print debug information to stdout
 
-#### Download scifact.zip dataset and unzip the dataset
+
+
 dataset = "mmarco"
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = here("datasets")
 data_path = util.download_and_unzip(url, out_dir)
 
-#### Provide the data path where scifact has been downloaded and unzipped to the data loader
-# data folder would contain these files: 
-# (1) scifact/corpus.jsonl  (format: jsonlines)
-# (2) scifact/queries.jsonl (format: jsonlines)
-# (3) scifact/qrels/test.tsv (format: tsv ("\t"))
-
 corpus, queries, qrels = GenericDataLoader(data_path+"/indonesian").load(split="dev")
 
-#### Lexical Retrieval using Bm25 (Elasticsearch) ####
-#### Provide a hostname (localhost) to connect to ES instance
-#### Define a new index name or use an already existing one.
-#### We use default ES settings for retrieval
-#### https://www.elastic.co/
+hostname = "localhost" 
+index_name = "mmarco-indo" 
 
-hostname = "localhost" #localhost
-index_name = "mmarco-indo" # germanquad
+initialize = False 
 
-#### Intialize #### 
-# (1) True - Delete existing index and re-index all documents from scratch 
-# (2) False - Load existing index
-initialize = False # False
+language = "indonesian" 
 
-#### Language ####
-# For languages supported by Elasticsearch by default, check here ->
-# https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-lang-analyzer.html
-language = "indonesian" # Please provide full names in lowercase for eg. english, hindi ...
-
-#### Sharding ####
-# (1) For datasets with small corpus (datasets ~ < 5k docs) => limit shards = 1 
 number_of_shards = 1
 model = BM25(index_name=index_name, hostname=hostname, language=language, initialize=initialize, number_of_shards=number_of_shards)
-
-# (2) For datasets with big corpus ==> keep default configuration
-# model = BM25(index_name=index_name, hostname=hostname, initialize=initialize)
 retriever = EvaluateRetrieval(model)
 
-#### Retrieve dense results (format of results is identical to qrels)
 results = retriever.retrieve(corpus, queries)
 
-#### Evaluate your retrieval using NDCG@k, MAP@K ...
 logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
 ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
 
@@ -68,7 +42,6 @@ recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metri
 hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
 
 
-#### Retrieval Example ####
 query_id, scores_dict = random.choice(list(results.items()))
 logging.info("Query : %s\n" % queries[query_id])
 
---
+++ experiments/bm25/eval_mrtydi.py
@@ -0,0 +1,51 @@
+from beir import util, LoggingHandler
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.lexical import BM25Search as BM25
+
+import pathlib, os, random
+import logging
+from pyprojroot import here
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+
+
+dataset = "mrtydi"
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+out_dir = here("datasets")
+data_path = util.download_and_unzip(url, out_dir)
+
+corpus, queries, qrels = GenericDataLoader(data_path+"/indonesian").load(split="dev")
+
+hostname = "localhost" 
+index_name = "mrtydi-indo" 
+
+initialize = False 
+
+language = "indonesian" 
+
+number_of_shards = 1
+model = BM25(index_name=index_name, hostname=hostname, language=language, initialize=initialize, number_of_shards=number_of_shards)
+retriever = EvaluateRetrieval(model)
+
+results = retriever.retrieve(corpus, queries)
+
+logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
+ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+
+mrr = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="mrr")
+recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="r_cap")
+hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
+
+
+query_id, scores_dict = random.choice(list(results.items()))
+logging.info("Query : %s\n" % queries[query_id])
+
+scores = sorted(scores_dict.items(), key=lambda item: item[1], reverse=True)
+for rank in range(10):
+    doc_id = scores[rank][0]
+    logging.info("Doc %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
